45d403345aa1ae9006c469f853951e7d446edcae
Nicolas Esmite <nicolasemanuelesmitevaldez@gmail.com>
2024-03-18 20:14:50 -0300

    maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -4,146 +4,101 @@ import "./index.css";
 function App() {
   return (
     <div className="min-h-full">
-      <nav className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <h2 className="text-amber-800	 font-mono text-2xl ">
-                  F1-anaticos
-                </h2>
-              </div>
-              <div className="hidden md:block">
-                <div className="relative inline-block text-left">
-                  <div>
-                    <button
-                      type="button"
-                      className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-                      id="menu-button"
-                      aria-expanded="true"
-                      aria-haspopup="true"
-                    >
-                      Options
-                      <svg
-                        className="-mr-1 h-5 w-5 text-gray-400"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                  <div
-                    className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="menu-button"
-                    tabindex="-1"
-                  >
-                    <div className="py-1" role="none">
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-0"
-                      >
-                        Edit
-                      </a>
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-1"
-                      >
-                        Duplicate
-                      </a>
-                    </div>
-                    <div className="py-1" role="none">
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-2"
-                      >
-                        Archive
-                      </a>
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-3"
-                      >
-                        Move
-                      </a>
-                    </div>
-                    <div className="py-1" role="none">
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-4"
-                      >
-                        Share
-                      </a>
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-5"
-                      >
-                        Add to favorites
-                      </a>
-                    </div>
-                    <div className="py-1" role="none">
-                      <a
-                        href="#"
-                        className="text-gray-700 block px-4 py-2 text-sm"
-                        role="menuitem"
-                        tabindex="-1"
-                        id="menu-item-6"
-                      >
-                        Delete
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="md:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Año de competencia
-            </a>
-          </div>
-        </div>
-      </nav>
-
-      <header className="bg-white shadow">
+      <header className="bg-rose-700">
         <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
           <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-            Dashboard
+            F1-anaticos
           </h1>
         </div>
       </header>
       <main>
-        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"></div>
+        <div className="h-dvh bg-slate-900">
+          <div className="flex h-3/4">
+            <div className="grid grid-cols-2 w-1/6 text-center mt-6 ml-2 p-3 border-double border-4 border-rose-600">
+              <a href="#" className="text-gray-50  ">
+                2002
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2003
+              </a>
+
+              <a href="#" className="text-gray-50  ">
+                2004
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2005
+              </a>
+
+              <a href="#" className="text-gray-50  ">
+                2006
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2007
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2008
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2009
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2010
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2011
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2012
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2013
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2014
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2015
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2016
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2017
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2018
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2019
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2020
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2021
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2022
+              </a>
+              <a href="#" className="text-gray-50  ">
+                2023
+              </a>
+            </div>
+
+            <div className="flex w-5/6 mt-6 mr-2 border-double border-4 border-rose-600">
+              <div className="w-3/12 h-full bg-gray-50"></div>
+              <div
+                className="flex
+               h-full w-6/12 bg-gray-300 gap-1"
+              >
+                <div className="w-4/12 bg-gray-500 h-5/6"></div>
+                <div className="w-4/12 bg-gray-500 h-5/6"></div>
+                <div className="w-4/12 bg-gray-500 h-5/6"></div>
+              </div>
+              <div className=" h-full"></div>
+            </div>
+          </div>
+        </div>
       </main>
     </div>
   );
