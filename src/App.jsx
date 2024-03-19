0001c2edd34b8cf284d15824e9e07bbef0ec3875
Nicolas Esmite <nicolasemanuelesmitevaldez@gmail.com>
2024-03-19 18:02:21 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,32 @@
 import "./index.css";
 
 import AppContainer from "./AppContainer";
+import { useEffect } from "react";
 
 function App() {
   const url = "https://ergast.com/api/f1/2003.json";
 
-  async function fetching(url) {
-    try {
-      const response = await fetch(url);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
+  useEffect(() => {
+    async function fetching(url) {
+      try {
+        const response = await fetch(url);
+        const result = await response.text();
+        console.log(result);
+      } catch (error) {
+        console.error(error);
+      }
+
+      return result;
     }
 
-    return result;
-  }
+    async function fetchByYear(year) {
+      const url = "https://ergast.com/api/f1/" + `${year}` + ".json";
+      console.log(url);
+      const result = await fetching(url);
 
-  async function fetchByYear(year) {
-    const url = "https://ergast.com/api/f1/" + `${year}` + ".json";
-    console.log(url);
-    const result = await fetching(url);
-
-    return result;
-  }
+      return result;
+    }
+  }, []);
 
   return <AppContainer datos={""} />;
 }
